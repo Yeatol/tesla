5ad156db7795a9d46ebff49549a4664b5e05f014
yeatol <vim@vimer.org>
2025-04-18 08:18:31 +0800

    data

--- vehicle_data.py
+++ vehicle_data.py
@@ -1,5 +1,6 @@
+import time
 import asyncio
-from tesla_fleet_api import TeslaBluetooth
+from tesla_fleet_api import TeslaBluetooth, BluetoothVehicleData
 
 async def main():
     vin = "LRWYGCFJ7MC076154"
@@ -7,7 +8,10 @@ async def main():
     private_key = await tesla_bluetooth.get_private_key()
     vehicle = tesla_bluetooth.vehicles.create(vin)
     device = await vehicle.find_vehicle()
-    await vehicle.pair()
-    print(f"Paired with VehicleBluetooth instance for VIN: {vehicle.vin}")
+    print(f"Vehicle data for VIN: {vehicle.vin}")
+    for i in range(100):
+        data = await vehicle.vehicle_data(["charge_state", "drive_state"])
+        print(data)
+        time.sleep(0.5)
 
 asyncio.run(main())
